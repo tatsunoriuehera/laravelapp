af6e27e2f0de9fdec0d83f8433605732a9f00c6d
tatsunoriuehera <uehara.t29@gmail.com>
2021-03-08 16:49:27 +0900

    first commit

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+
+class HelloController extends Controller
+{
+    //
+    /*
+    public function index($id='noname',$pass='unknown'){
+      return <<< EOF
+      <html>
+      <head>
+      <title>hello index</title>
+      </head>
+      <body><p>hello controller index action</p></body>
+      </html>
+      EOF;
+    }
+    */
+
+    public function index(){
+      //$data = ['msg'=>'this is controller send message'];
+      return view('hello.index');
+    }
+
+    public function other(){
+      $str = <<<EOD
+これはotherです。
+EOD;
+
+// 変数を出力
+echo $str;
+    }
+    public function __invoke(){
+      $str = <<<EOD
+これはinvokeです。<br>
+シングルアクションコントローラ
+EOD;
+
+// 変数を出力
+echo $str;
+    }
+
+/*
+    public function index(Request $request, Response $response){
+      $html = <<<EOD
+      {$request}</br>
+      {$response}</br>
+これはrequest and responseです。
+EOD;
+
+      $response->setContent($html);
+      return $response;
+    }
+*/
+}
---
+++ resources/views/hello/index.php
@@ -0,0 +1,10 @@
+<html>
+<head></head>
+<body>
+<p>
+this is sample page. php template
+<?php //echo $msg;?>
+<?php //echo date("y");?>
+</p>
+</body>
+</html>
